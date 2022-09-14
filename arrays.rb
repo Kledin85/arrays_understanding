52a2b4523c6a393e1082d981781265a9e097be77
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-09-14 10:17:16 -0500

    call one of each array and use a different method

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,19 @@ ammount = [34, 45, 87, 98]
 prices = [9.99, 12.85, 10.75, 8.50]
 
 is_instock = [true, false, true, true]
+
+# this method will remove the last element
+
+foods.pop
+
+# this methos will add an element to the end of the array
+
+ammount.push 99
+
+# this method will add an element to the front of the array
+
+prices.unshift 7.25
+
+# this method will remove an element from the front of the array
+
+is_instock.shift
