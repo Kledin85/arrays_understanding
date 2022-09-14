38b16b47fda2c75ee488a491235aff4feac065f2
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-09-14 10:22:28 -0500

    add an explanation for index positions

--- arrays.rb
+++ arrays.rb
@@ -6,18 +6,20 @@ prices = [9.99, 12.85, 10.75, 8.50]
 
 is_instock = [true, false, true, true]
 
-# this method will remove the last element
+# This method will remove the last element
 
 foods.pop
 
-# this methos will add an element to the end of the array
+# This methos will add an element to the end of the array
 
 ammount.push 99
 
-# this method will add an element to the front of the array
+# This method will add an element to the front of the array
 
 prices.unshift 7.25
 
-# this method will remove an element from the front of the array
+# This method will remove an element from the front of the array
 
 is_instock.shift
+
+# The index postion starts with a 0. In the foods array index 0 would be Burger.
